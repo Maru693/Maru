b1e72ef6273acaf5d32fef152cada61b481af43f
Maru693 <jp.maru0318@outlook.jp>
2025-02-22 06:17:18 +0000

    適切なコミットメッセージ

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
-use App\Http\Controllers\PostController; 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController; 
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/posts', [PostController::class, 'index']); 
+
+Route::get('/', [PostController::class, 'index']); 
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
+Route::post('/posts', [PostController::class, 'store']);
+// '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
